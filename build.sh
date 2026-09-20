4a693e9c5af89b71b9b695a9e3119d1fe756691b
Syuugo <pub@s1204.me>
2026-09-21 04:37:27 +0900

    Update deps (#3)
    Signed-off-by: Syuugo <pub@s1204.me>

--- build.sh
+++ build.sh
@@ -1,21 +1,27 @@
 #/bin/bash
 
+# Ignore JVM warnings
+export JAVA_TOOL_OPTIONS="--enable-native-access=ALL-UNNAMED"
+
 # Clean
 rm -f stub-unaligned.apk stub.apk stub.apk.idsig
 
-# Value of minSdk
-MIN_SDK="${1:-21}"
+# Value of minSdk (Default: 24)
+MIN_SDK="${1:-24}"
+
+# Value of targetSdk (Default: 37)
+TARGET_SDK="${2:-37}"
 
 # Make APK
-aapt package -M AndroidManifest.xml -I $ANDROID_HOME/platforms/android-35/android.jar -F stub-unaligned.apk --min-sdk-version $MIN_SDK --target-sdk-version 35
+aapt package -M AndroidManifest.xml -I $ANDROID_SDK_ROOT/platforms/android-37.2/android.jar -F stub-unaligned.apk --min-sdk-version $MIN_SDK --target-sdk-version $TARGET_SDK
 
 # Alingn APK
 zipalign 4 stub-unaligned.apk stub.apk
 rm stub-unaligned.apk
 
 # Sign APK
-apksigner sign --cert data/cert.pem --key data/cert.pk8 --v1-signing-enabled true --v2-signing-enabled false --v3-signing-enabled false --v4-signing-enabled false stub.apk
+apksigner sign --cert data/cert.pem --key data/cert.pk8 stub.apk
 
-rm stub.apk.idsig
+rm -f stub.apk.idsig
 
 echo "Created: stub.apk"
