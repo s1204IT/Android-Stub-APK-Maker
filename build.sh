bd10e0a6d7f378380be786ae831ac462ac84b09a
Syuugo <pub@s1204.me>
2026-09-21 04:26:10 +0900

    Upgrade default targetSdk version
    Signed-off-by: Syuugo <pub@s1204.me>

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ export JAVA_TOOL_OPTIONS="--enable-native-access=ALL-UNNAMED"
 # Clean
 rm -f stub-unaligned.apk stub.apk stub.apk.idsig
 
-# Value of minSdk
-MIN_SDK="${1:-21}"
+# Value of targetSdk (Default: 35)
+TARGET_SDK="${1:-35}"
 
 # Make APK
-aapt package -M AndroidManifest.xml -I $ANDROID_HOME/platforms/android-37.2/android.jar -F stub-unaligned.apk --min-sdk-version $MIN_SDK --target-sdk-version $MIN_SDK
+aapt package -M AndroidManifest.xml -I $ANDROID_SDK_ROOT/platforms/android-37.2/android.jar -F stub-unaligned.apk --min-sdk-version $TARGET_SDK --target-sdk-version $TARGET_SDK
 
 # Alingn APK
 zipalign 4 stub-unaligned.apk stub.apk
