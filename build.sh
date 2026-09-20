c92842ba2891552b67282d6169810efea484a23d
Syuugo <pub@s1204.me>
2026-09-21 05:47:47 +0900

    Optimize signing level

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #/bin/bash
 
-# Ignore JVM warnings
-export JAVA_TOOL_OPTIONS="--enable-native-access=ALL-UNNAMED"
-
 # Clean
 rm -f stub-unaligned.apk stub.apk stub.apk.idsig
 
@@ -17,11 +14,16 @@ aapt package -M AndroidManifest.xml -I $ANDROID_SDK_ROOT/platforms/android-37.2/
 
 # Alingn APK
 zipalign 4 stub-unaligned.apk stub.apk
-rm stub-unaligned.apk
+rm -f stub-unaligned.apk
+
+# Disable v2 signing
+SIG_V2=""
+if [ $TARGET_SDK -le 29 ]; then
+  SIG_V2="--v1-signing-enabled true --v2-signing-enabled false --v3-signing-enabled false"
+fi
 
 # Sign APK
-apksigner sign --cert data/cert.pem --key data/cert.pk8 stub.apk
-
+apksigner sign --cert data/cert.pem --key data/cert.pk8 $SIG_V2 stub.apk
 rm -f stub.apk.idsig
 
 echo "Created: stub.apk"
